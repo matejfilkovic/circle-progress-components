9eb7f9d921f1b7ba9c254c1153f6911a2bf9c20c
Matej Filkovic <matej.filkovic@gmail.com>
2018-01-07 20:30:42 +0100

    Add CircleProgressThinBorder

---
+++ src/components/circle-progress-thin-border/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import CircleProgress from '../circle-progress'
+
+export function CircleProgressThinBorder(props) {
+  const {
+    percentageToFill,
+    text,
+    baseCircleColor,
+    progressCircleColor
+  } = props
+
+  return (
+    <CircleProgress
+      percentageToFill={percentageToFill}
+      text={text}
+      baseCircleColor={baseCircleColor}
+      progressCircleColor={progressCircleColor}
+      radius={31.25}
+      strokeWidth={5}
+      textY={37.5}
+      textStrokeWidth={2}
+      textFontSize={18}
+    />
+  )
+}
+
+/* eslint-disable react/require-default-props */
+CircleProgressThinBorder.propTypes = {
+  percentageToFill: PropTypes.number.isRequired,
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  baseCircleColor: PropTypes.string,
+  progressCircleColor: PropTypes.string
+}
